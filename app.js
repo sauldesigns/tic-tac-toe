3084127dd815936562f3135e52bc1d43faa867c8
Saul Designs <saul.pla1221@gmail.com>
2021-07-09 20:09:22 -0700

    init

---
+++ app.js
@@ -0,0 +1,14 @@
+// HTML Elements
+
+const statusDiv = document.querySelector('.status');
+
+const resetButton = document.querySelector('.reset');
+
+const cellDivs = document.querySelectorAll('.game-cell');
+
+// Game Variables
+
+let gameIsStarted = true;
+let xIsNext = true; // if x turn then true if false then o's turn
+
+// event listeners
